c34ca846e2509c4667dd58c0f7b4b7471179f966
xiaoyaoyao1 <704410170@qq.com>
2020-02-17 22:38:20 +0800

    初步实现阅读数功能以及json数据编写

---
+++ src/main/java/com/yzc/forum/controller/CommentController.java
@@ -0,0 +1,33 @@
+package com.yzc.forum.controller;
+
+import com.yzc.forum.dto.CommentDTO;
+import com.yzc.forum.mapper.CommentMapper;
+import com.yzc.forum.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        return null;
+    }
+}
